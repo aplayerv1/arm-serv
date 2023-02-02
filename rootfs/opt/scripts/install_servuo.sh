e43949d20ec5a23ccdbd3b4405b3cac2aa305fd0
root <hoyhoy100@hotmail.com>
2023-02-01 20:45:07 -0500

    fix error

--- rootfs/opt/scripts/install_servuo.sh
+++ rootfs/opt/scripts/install_servuo.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/with-contenv bash
+FILE=/opt/ServUO/ServUO.exe
 
 echo "================================================================================"
 echo "Running install_serv.sh as $(whoami)".
 echo "================================================================================"
 apt-get update 
-apt-get install -y mono-devel 
+apt-get install -y mono-devel rsync nano
 echo ""
 echo "================================================================================"
 echo "ServUO git check"
 echo "================================================================================"
 echo ""
-if [ ! -d "${_PATH_INSTALL_ROOT}" ] ; then
-    git clone https://github.com/ServUO/ServUO.git ${_PATH_INSTALL_ROOT}
-else
-    cd "${_PATH_INSTALL_ROOT}"
-    git pull https://github.com/ServUO/ServUO.git
+echo "tmp"
+cd /root/
+if [ ! -f /opt/ServUO/ServUO.exe ]; then
+    git clone https://github.com/ServUO/ServUO.git
+    rsync -av /root/ServUO /opt
+    rm -r /root/ServUO
+    echo "tmp done"
+    echo ""
+    echo "================================================================================"
+    echo "BAKING"
+    echo "================================================================================"
+    echo ""
+    cd /opt/ServUO
+    export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+    export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools
+    echo ""
+    echo "================================================================================"
+    echo "ORANGE"
+    echo "================================================================================"
+    dotnet build || exit 6;
 fi
-echo ""
-echo "================================================================================"
-echo "BAKING"
-echo "================================================================================"
-echo ""
-cd ${_PATH_INSTALL_ROOT} 
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
-export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools
-echo ""
-echo "================================================================================"
-echo "ORANGE"
-echo "================================================================================"
-dotnet build || exit 6;
 exit 0;
