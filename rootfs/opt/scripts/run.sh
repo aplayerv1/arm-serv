ca750a0349142d995e78b92659fdb2201471b214
root <hoyhoy100@hotmail.com>
2023-07-12 07:29:22 -0400

    updatelinks

--- rootfs/opt/scripts/run.sh
+++ rootfs/opt/scripts/run.sh
@@ -5,7 +5,7 @@ echo "Running ServUO"
 echo "================================================================================"
 echo ""
 if [ ! -f /opt/data/fonts.mul ]; then
-wget  https://mvia.ca/data.zip -P /tmp
+wget  https://other.mvia.ca/data.zip -P /tmp
 cd /tmp
 unzip data.zip -d /opt/data
 fi
